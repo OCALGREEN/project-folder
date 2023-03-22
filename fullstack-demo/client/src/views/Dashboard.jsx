987cd9d12401efcd04a2faf8b00ce7795e7d1f34
Oger Carrillo <ogercarrillo@gmail.com>
2023-03-22 14:07:32 -0700

    dashboard get all done

--- fullstack-demo/client/src/views/Dashboard.jsx
+++ fullstack-demo/client/src/views/Dashboard.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+// grab all the pets from the backend 
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios' 
 
 const Dashboard = () => {
+    
+    const [pets, setPets] = useState() 
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/pets`)
+            .then(res => setPets(res.data))
+            .catch(err => console.log(err)) 
+    }, [])
+
     return (
         <fieldset>
             <legend> Dashboard.jsx </legend>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Pet Name</th>
+                        <th>Hair Color</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        pets && 
+                            pets.map((pet, i) => (
+                                <tr key={i}>
+                                    <td>{ pet.petName }</td>
+                                    <td>{ pet.hairColor }</td>
+                                    <td>{ pet.age }</td>
+                                </tr>
+                            ))
+                    }
+                </tbody>
+            </table>
         </fieldset>
     )
 }
