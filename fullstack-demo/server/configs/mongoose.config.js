c6f54ed7fc1003a74b8c06f48e157671ad03579c
Oger Carrillo <ogercarrillo@gmail.com>
2023-03-22 10:32:38 -0700

    updated config mongoose

--- fullstack-demo/server/configs/mongoose.config.js
+++ fullstack-demo/server/configs/mongoose.config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 // connect to the db 
 mongoose.connect("mongodb://localhost/pets_db", {
-    userNewUrlParser: true, 
+    useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
     .then(() => console.log("Established a connection to the database")) 
